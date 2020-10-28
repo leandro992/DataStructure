2cabcb64d85f5161c078f52ae081997bc61e7df1
Victor Leandro <victorleandro100@outlook.com>
2020-10-27 22:26:30 -0300

    class man

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,12 @@
 package com.company;
 
-import com.company.data.structure.ed.Vector;
-import com.company.data.structure.entity.Student;
+import com.company.data.structure.ed.Queue;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Student a1 = new Student("João");
+       /* Student a1 = new Student("João");
         Student a2 = new Student("Maria");
         Student a3 = new Student("Raquel");
         Student a4 = new Student("Samuel");
@@ -21,7 +20,41 @@ public class Main {
         System.out.println(list.contains(a3));
         System.out.println(list);
         System.out.println("Text");
-        System.out.println(list.takes(200));
+        System.out.println(list.takes(200));*/
+
+        
+        //----------------- Test class Stack --------------------------------------------
+
+
+     /*   Stack stack = new Stack();
+
+        stack.push("São Paulo");
+        System.out.println(stack);
+        stack.push("Santos");
+        System.out.println(stack);
+
+        String r1 = stack.pop();
+        System.out.println(r1);
+        String r2 = stack.pop();
+        System.out.println(r2);
+
+        System.out.println(stack);*/
+
+
+        //---------------------- TestQueue ---------------------------------------
+
+        Queue queue = new Queue();
+
+        String nome = "Santos ";
+        nome.hashCode();
+
+        queue.insert("João");
+        System.out.println(queue);
+
+        queue.insert("Marcelo");
+        System.out.println(queue);
+
+
 
     }
 
---
+++ src/com/company/data/structure/ed/Stack.java
@@ -0,0 +1,2 @@
+package com.company.data.structure.ed;public class Stack {
+}
